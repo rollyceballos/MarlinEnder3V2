ed37d0348f91d2d2e36369ab33e3a6574fa426f7
Rolly <rollyceballos@me.com>
2020-10-15 17:10:21 +0800

    Customize PLA and ABS presets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1499,14 +1499,14 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 185
-#define PREHEAT_1_TEMP_BED     45
+#define PREHEAT_1_TEMP_HOTEND 200
+#define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED     70
-#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+#define PREHEAT_2_FAN_SPEED   25 // Value from 0 to 255
 
 /**
  * Nozzle Park
